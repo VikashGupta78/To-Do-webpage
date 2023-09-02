acb6b326db09ed870db5ce1dcbe963748aac6adc
Vikash Kumar Gupta <vikashkg12@gmail.com>
2023-09-02 10:59:06 +0530

    jus started

---
+++ config/database.js
@@ -0,0 +1,23 @@
+
+const mongoose = require('mongoose');
+
+require("dotenv").config();
+
+// Replace 'your_mongodb_url' with your actual MongoDB connection string
+const dbConnect = () => {
+    // Connect to MongoDB
+    mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('Connected to MongoDB!'))
+    .catch((error) => {
+        console.log('Issue in Db connection');
+        console.error(error.message);
+        process.exit(1);
+    })
+
+}
+
+module.exports = dbConnect;
+
---
+++ models/ToDo.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.schema(
+    {
+        title:{
+            type: String,
+            required: true,
+            maxL
+        }
+
+    }
+)
